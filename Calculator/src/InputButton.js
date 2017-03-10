db5a414fb4e2ae7599baea91d3b1d0efd8f07850
Sachin Gupta <hapysachin91@gmail.com>
2017-03-10 14:01:01 -0800

    adding TouchableHighlight view in input Button component

--- Calculator/src/InputButton.js
+++ Calculator/src/InputButton.js
@@ -1,21 +1,27 @@
 // InputButton.js
+/**
+ * The Text view uses this.props.value. Props are essentially static data that we can pass to child components,
+ *  as we’ll see when we return to the Calculator class.
+ * 
+ */
 
 import React, { Component } from 'react';
 import {
-    View,
+    TouchableHighlight,
     Text
 } from 'react-native';
 
 import Style from './Style';
 
 export default class InputButton extends Component {
-    
-    render() {
+
+    render(){
         return (
-            <View style={Style.inputButton}>
+            <TouchableHighlight style={Style.inputButton} 
+                                underlayColor="#193441"
+                                onPress={this.props.onPress}>
                 <Text style={Style.inputButtonText}>{this.props.value}</Text>
-            </View>
-        )
+            </TouchableHighlight>
+        );
     }
-    
-}
+} 
