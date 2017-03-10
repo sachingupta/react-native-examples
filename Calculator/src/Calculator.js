085fb4dee63fa9abda33078cc3cafcaabcac47f6
Sachin Gupta <hapysachin91@gmail.com>
2017-03-10 00:30:58 -0800

    adding input component and adding calculater input buttons

--- Calculator/src/Calculator.js
+++ Calculator/src/Calculator.js
@@ -5,15 +5,54 @@ import {
   AppRegistry
 } from 'react-native';
 
+import Style from './Style';
+
+import InputButton from './InputButton';
+
+// Define the input buttons that will be displayed in the calculator.
+const inputButtons = [
+    [1, 2, 3, '/'],
+    [4, 5, 6, '*'],
+    [7, 8, 9, '-'],
+    [0, '.', '=', '+']
+];
+
+/* <View style={{flex: 8, backgroundColor: '#3E606F'}}></View> */
 class Calculator extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-            <View style={{flex: 2, backgroundColor: '#193441'}}></View>
-            <View style={{flex: 8, backgroundColor: '#3E606F'}}></View>
+      <View style={Style.rootContainer}>
+            <View style={Style.displayContainer}></View>
+            <View style={Style.inputContainer}>
+                 {this._renderInputButtons()}
+            </View>  
         </View>
     )
   }
+
+  /**
+     * For each row in `inputButtons`, create a row View and add create an InputButton for each input in the row.
+     */
+    _renderInputButtons() {
+        let views = [];
+
+        for (var r = 0; r < inputButtons.length; r ++) {
+            let row = inputButtons[r];
+
+            let inputRow = [];
+            for (var i = 0; i < row.length; i ++) {
+                let input = row[i];
+
+                inputRow.push(
+                    <InputButton value={input} key={r + "-" + i} />
+                );
+            }
+
+            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+        }
+
+        return views;
+    }
 }
 
 AppRegistry.registerComponent('Calculator', () => Calculator);
