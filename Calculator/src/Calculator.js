cd0b990b993761824ba371ebda369b84853703ba
Sachin Gupta <hapysachin91@gmail.com>
2017-03-10 14:01:55 -0800

    adding function to handle input button click for number buttons

--- Calculator/src/Calculator.js
+++ Calculator/src/Calculator.js
@@ -10,21 +10,37 @@ import Style from './Style';
 import InputButton from './InputButton';
 
 // Define the input buttons that will be displayed in the calculator.
-const inputButtons = [
+const Buttons = [
     [1, 2, 3, '/'],
     [4, 5, 6, '*'],
     [7, 8, 9, '-'],
     [0, '.', '=', '+']
 ];
 
-/* <View style={{flex: 8, backgroundColor: '#3E606F'}}></View> */
+/* <View style={{flex: 8, backgroundColor: '#3E606F'}}></View> 
+State allows us to update the UI of our applications based on dynamic data. 
+The first thing we’ll use State for is to update the display based on the numbers entered by the user.
+During the constructor is the only time you can modify state directly, after which it must be deemed immutable and 
+can only be modified using setState, as we’ll see in a moment.
+*/
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        inputValue: 0
+    };
+  }
+
   render() {
     return (
       <View style={Style.rootContainer}>
-            <View style={Style.displayContainer}></View>
+            <View style={Style.displayContainer}>
+                <Text style={Style.displayText}>
+                    {this.state.inputValue}
+                </Text>
+            </View>
             <View style={Style.inputContainer}>
-                 {this._renderInputButtons()}
+                 {this.renderInputButtons(Buttons)}
             </View>  
         </View>
     )
@@ -32,26 +48,47 @@ class Calculator extends Component {
 
   /**
      * For each row in `inputButtons`, create a row View and add create an InputButton for each input in the row.
+     * Note: The key on the Components within _renderInputButtons is required when you are creating an array of Components, 
+     * and must be unique to each Component in the array.
      */
-    _renderInputButtons() {
-        let views = [];
+    renderInputButtons(inputButtons) {
+        let viewContainer = [];
 
-        for (var r = 0; r < inputButtons.length; r ++) {
-            let row = inputButtons[r];
-
+        for(let row=0; row< inputButtons.length; row++){
             let inputRow = [];
-            for (var i = 0; i < row.length; i ++) {
-                let input = row[i];
+            let rowButtons = inputButtons[row];
 
+            for(let col=0;col<rowButtons.length;col++){
+                let input = rowButtons[col];
                 inputRow.push(
-                    <InputButton value={input} key={r + "-" + i} />
+                    <InputButton value={input}
+                                 onPress={this.onInputButtonPressed.bind(this, input)}
+                                 key={row + "-" + col} /  >
                 );
             }
 
-            views.push(<View style={Style.inputRow} key={"row-" + r}>{inputRow}</View>)
+            viewContainer.push(
+                <View style={Style.inputRow} key={"row-" + row}>
+                    {inputRow} 
+                </View> 
+             );
         }
+        return viewContainer;
+    }
 
-        return views;
+    onInputButtonPressed(input){
+        switch( typeof input){
+            case 'number':
+                return this.handleNumberInput(input);
+        }
+    }
+
+    handleNumberInput(num){
+        let newInputValue = (this.state.inputValue * 10) + num;
+
+        this.setState({
+                inputValue: newInputValue
+        });
     }
 }
 
